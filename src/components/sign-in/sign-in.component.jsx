95a7faa3729d97b13837955655adabf844cc70ce
Arnab Gupta <guptaarnab639@gmail.com>
2020-07-12 18:49:08 +0530

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import "./sign-in.styles.scss";
-import FormInput from "../form-input/form-input.componen";
+import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,40 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-	apiKey: "AIzaSyB0kxhtYl5_PHVz9eQ8zoCClCzUn78JyhU",
-	authDomain: "cloth-db-97873.firebaseapp.com",
-	databaseURL: "https://cloth-db-97873.firebaseio.com",
-	projectId: "cloth-db-97873",
-	storageBucket: "cloth-db-97873.appspot.com",
-	messagingSenderId: "451241081408",
-	appId: "1:451241081408:web:f43b5d9f86b0deae1ffda5",
-	measurementId: "G-PCEJWKVQ6J"
+  apiKey: "AIzaSyB0kxhtYl5_PHVz9eQ8zoCClCzUn78JyhU",
+  authDomain: "cloth-db-97873.firebaseapp.com",
+  databaseURL: "https://cloth-db-97873.firebaseio.com",
+  projectId: "cloth-db-97873",
+  storageBucket: "cloth-db-97873.appspot.com",
+  messagingSenderId: "451241081408",
+  appId: "1:451241081408:web:f43b5d9f86b0deae1ffda5",
+  measurementId: "G-PCEJWKVQ6J",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error creating user ", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
