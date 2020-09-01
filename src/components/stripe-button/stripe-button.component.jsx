f86afc8dd2bf9455ee9c279d70d358e1384b4451
Arnab Gupta <guptaarnab639@gmail.com>
2020-09-01 13:25:54 +0530

    changed to JS styled components

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,27 +1,26 @@
-import React from "react";
-import StripeCheckout from "react-stripe-checkout";
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey =
-    "pk_test_51HK2jDCqVh3PAw3aCoCAP5Qf77wB37Up8QcTpi3morrKIHRXUnfjHCiuguoAJYJRjYJt5AY6vWBfpYCnnruaQUEH00wYuJDvDx";
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
-  const ontoken = (token) => {
+  const onToken = token => {
     console.log(token);
-    alert("Payment successful");
+    alert('Payment Succesful!');
   };
 
   return (
     <StripeCheckout
-      label="Pay Now"
-      name="Cloth Ltd."
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/up/d/f3eb2117da"
+      image='https://svgshare.com/i/CUz.svg'
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel="Pay Now"
-      token={ontoken}
+      panelLabel='Pay Now'
+      token={onToken}
       stripeKey={publishableKey}
     />
   );
